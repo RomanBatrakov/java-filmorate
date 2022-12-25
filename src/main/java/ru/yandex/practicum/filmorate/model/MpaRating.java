5dd56390c7c1f56a0256763162102ca37bdcfad5
Roman_B <batrakov.r.v@yandex.ru>
2022-12-25 10:05:48 +0300

    refactor

--- src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MpaRating {
 
     private int id;
